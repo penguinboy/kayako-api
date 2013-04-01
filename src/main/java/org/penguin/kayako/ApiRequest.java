c3335a9f61a3d566df828ba87cc5d1e5bed3356f
William Rayner <will.rayner@papercut.com>
2013-04-01 15:13:10 +1100

    Now using spaces instead of tabs

--- src/main/java/org/penguin/kayako/ApiRequest.java
+++ src/main/java/org/penguin/kayako/ApiRequest.java
@@ -8,47 +8,47 @@ import javax.crypto.spec.SecretKeySpec;
 import sun.misc.BASE64Encoder;
 
 public class ApiRequest {
-	private final String apiSecret;
-	private String salt;
-	private String signature;
-	
-	public ApiRequest(String apiSecret) throws ApiRequestException {
-		this.apiSecret = apiSecret;
-		
-		SecureRandom r = new SecureRandom();
-		StringBuilder saltBuilder = new StringBuilder();
-		for (int i = 0; i < 7; i++) {
-	        saltBuilder.append((char) (97 + r.nextInt(26)));
-	    }
-		this.salt = saltBuilder.toString();
-		
-		this.generateSignature();
-	}
-	
-	public void setSalt(String salt) throws ApiRequestException {
-		this.salt = salt;
-		generateSignature();
-	}
-	
-	private void generateSignature() throws ApiRequestException {
-		Mac hmac;
-		try {
-			hmac = Mac.getInstance("HmacSHA256");
-			hmac.init(new SecretKeySpec(apiSecret.getBytes(), "HmacSHA256"));
-			
-			this.signature = new BASE64Encoder().encode(hmac.doFinal(this.salt.getBytes("UTF-8")));
-		} catch (Exception e) {
-			throw new ApiRequestException(e);
-		}
-	}
-	
-	public String getSignature() {
-		return signature;
-	}
+    private final String apiSecret;
+    private String salt;
+    private String signature;
 
-	public static class ApiRequestException extends Exception {
-		public ApiRequestException(Throwable e) {
-			super("An exception occurred attempting to create API request", e);
-		}
-	}
+    public ApiRequest(String apiSecret) throws ApiRequestException {
+        this.apiSecret = apiSecret;
+
+        SecureRandom r = new SecureRandom();
+        StringBuilder saltBuilder = new StringBuilder();
+        for (int i = 0; i < 7; i++) {
+            saltBuilder.append((char) (97 + r.nextInt(26)));
+        }
+        this.salt = saltBuilder.toString();
+
+        this.generateSignature();
+    }
+
+    public void setSalt(String salt) throws ApiRequestException {
+        this.salt = salt;
+        generateSignature();
+    }
+
+    private void generateSignature() throws ApiRequestException {
+        Mac hmac;
+        try {
+            hmac = Mac.getInstance("HmacSHA256");
+            hmac.init(new SecretKeySpec(apiSecret.getBytes(), "HmacSHA256"));
+
+            this.signature = new BASE64Encoder().encode(hmac.doFinal(this.salt.getBytes("UTF-8")));
+        } catch (Exception e) {
+            throw new ApiRequestException(e);
+        }
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public static class ApiRequestException extends Exception {
+        public ApiRequestException(Throwable e) {
+            super("An exception occurred attempting to create API request", e);
+        }
+    }
 }
--- src/test/java/org/penguin/kayako/ApiRequestTests.java
+++ src/test/java/org/penguin/kayako/ApiRequestTests.java
@@ -5,17 +5,17 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class ApiRequestTests {
-	
-	@Test
-	public void testSignatureGeneratesCorrectly() throws Exception {
-		// arrange
-		String apiSecret = "MDA4YzBiMWMtN2RiOC1hZTY0LTMxODgtMzE1MThjNmU5NDJlYTM1ZTgwY2YtYjA1ZS1jMzQ0LWY5MjktMzQ1ZjliMDA4ODIx";
-		
-		// act
-		ApiRequest request = new ApiRequest(apiSecret);
-		request.setSalt("3777329113");
-		
-		// assert
-		Assert.assertEquals("gFhgQ1Gydk+DLsn6BnHO/eqs1KxPwqmlj2bRWfjFFYs=", request.getSignature());
-	}
+
+    @Test
+    public void testSignatureGeneratesCorrectly() throws Exception {
+        // arrange
+        String apiSecret = "MDA4YzBiMWMtN2RiOC1hZTY0LTMxODgtMzE1MThjNmU5NDJlYTM1ZTgwY2YtYjA1ZS1jMzQ0LWY5MjktMzQ1ZjliMDA4ODIx";
+
+        // act
+        ApiRequest request = new ApiRequest(apiSecret);
+        request.setSalt("3777329113");
+
+        // assert
+        Assert.assertEquals("gFhgQ1Gydk+DLsn6BnHO/eqs1KxPwqmlj2bRWfjFFYs=", request.getSignature());
+    }
 }
