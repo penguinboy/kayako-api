bedc408a096ceb722d53cb8dfd4cbaf0a116c51e
Roman Romanchuk <romanr@crytek.de>
2013-04-26 15:13:23 +0200

    Added httpclient library for making requests (enable POST/PUT/DELETE methods)

--- src/main/java/org/penguin/kayako/ApiRequest.java
+++ src/main/java/org/penguin/kayako/ApiRequest.java
@@ -2,11 +2,16 @@ package org.penguin.kayako;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.security.SecureRandom;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import sun.misc.BASE64Encoder;
 
 import com.google.common.io.CharStreams;
@@ -58,8 +63,11 @@ public class ApiRequest {
             UriBuilder uriBuilder = uri.queryParam("apikey", apiKey)
                     .queryParam("salt", salt)
                     .queryParam("signature", signature);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(uriBuilder.toURL().openStream()));
-            String content = CharStreams.toString(reader);
+
+            HttpGet httpGet = new HttpGet(uriBuilder.toURI());
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpResponse response = httpClient.execute(httpGet);
+            String content = EntityUtils.toString(response.getEntity()) ;
             return new ApiResponse(content);
         } catch (Exception e) {
             // Gotta catch em all
